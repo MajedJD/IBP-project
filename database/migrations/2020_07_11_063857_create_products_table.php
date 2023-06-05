a2ad6f25814f02698e76f9748292d1a575501860
MajedJD <majedaljadi2@gmail.com>
2023-06-05 23:30:07 +0300

    upload

---
+++ database/migrations/2020_07_11_063857_create_products_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('summary');
+            $table->longText('description')->nullable();
+            $table->text('photo');
+            $table->integer('stock')->default(1);
+            $table->string('color')->nullable();
+            $table->unsignedBigInteger('condition_id');
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->float('price');
+            $table->float('discount')->nullable();
+            $table->boolean('is_featured')->default(false)->nullable();
+            $table->boolean('d_deal')->default(false);
+            $table->unsignedBigInteger('brand_id')->nullable();
+            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('SET NULL');
+            $table->timestamps();
+        });
+    }
+    
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign('products_condition_id_foreign');
+        });
+    }
+}
